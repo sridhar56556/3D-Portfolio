0a093a79d675608042fb8858294093fd80da2c77
Sridhr <sridharkonda553@gmail.com>
2026-05-21 14:36:27 +0530

    chore: configure base path and GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/3D-Portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
